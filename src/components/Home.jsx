84db191307501df2839631f7ccd2c86119ace390
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2025-08-29 19:43:50 +0200

    home
    Start working on home page and intrudiction card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./Home.css"
 
 export default function Home(){
     return(
-        <h2>Home Page!</h2>
+        <div className='home-container'>
+            <div className='card'>
+                <div className='card-body'>
+                    <h2>Home Page!</h2>
+                </div>
+            </div>
+            
+        </div>
     )
 }

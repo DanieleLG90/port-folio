5410de8086529da89cdf338c78aadb0bf58a29b1
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2025-08-30 19:23:03 +0200

    home page
    main structure for the home page done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./Home.css"
+import photoProfile from "../img/temp-profile-img.png"
 
 export default function Home(){
     return(
         <div className='home-container'>
             <div className='card'>
                 <div className='card-body'>
-                    <h2>Home Page!</h2>
+                    <div class="profile-image">
+                        <img src={photoProfile} alt="Foto del developer" />
+                    </div>
+                    <div class="intro-text">
+                        <h2>Hello</h2>
+                        <h1>Sono Daniele Lorenzo Giacomazzi</h1>
+                        <p>A passionate web developer!</p>
+                        <button class="view-work-button">View My Work</button>
+                    </div>
                 </div>
             </div>
             

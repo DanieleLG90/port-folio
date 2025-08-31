2ecfcd765624e1efb61f4d7205e1e970fd38b1a2
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2025-08-31 19:42:23 +0200

    header

--- src/head+foot/Header.jsx
+++ src/head+foot/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,NavLink, Link } from 'react-router-dom';
 import "./Header.css"
 
 export default function Header(){
     return(
         <header>
-         <h2>Header!</h2>
+            <div className='header-body'>
+                <Link className='header-home-link'>DLG</Link>
+                <div className='header-links'>
+                    <NavLink>Projects</NavLink>
+                    <NavLink>Contact</NavLink>
+                </div>
+            </div>
         </header>
     )
 }

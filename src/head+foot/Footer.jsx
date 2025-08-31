4e3e714a60ebe381d3b1b5670942b95ae1972a3d
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2025-08-31 20:43:57 +0200

    header+footer

--- src/head+foot/Footer.jsx
+++ src/head+foot/Footer.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import "./Footer.css"
 
 export default function Footer(){
     return(
         <footer>
-         <h2>Footer!</h2>
+            <div className='footer-body'>
+                <div className='social-links'>
+                    <a href="https://www.linkedin.com/in/[tuo-profilo]" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin className="social-icon" />
+                    </a>
+                    <a href="https://github.com/[tuo-profilo]" target="_blank" rel="noopener noreferrer">
+                        <FaGithub className="social-icon" />
+                    </a>
+                    <a href="mailto:[tua-email]">
+                        <FaEnvelope className="social-icon" />
+                    </a>
+                </div>
+                 <p className="copyright">
+                    © 2025 [Daniele Lorenzo Giacomazzi]
+                </p>
+            </div>
         </footer>
     )
 }

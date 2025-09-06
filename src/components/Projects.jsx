58f643e34d7ca128dba404f1ffa43cc80d63c975
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2025-09-06 12:49:30 +0200

    projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+import MainProject from './projects-components/MainProject';
+import SecondProject from './projects-components/SecondProject';
+import "./Projects.css"
+
 export default function Projects(){
     return(
-        <h2>Projects Page!</h2>
+        <div className='projects-container'>
+            
+            <div className='main-projects-container'>
+                <MainProject />
+                <MainProject />
+                <MainProject />
+                <MainProject />
+            </div>
+
+            <div className='secondary-projects-container'>
+                <SecondProject />
+                <SecondProject />
+                <SecondProject />
+                <SecondProject />
+                <SecondProject />
+            </div>
+            
+        </div>
+        
     )
 }
---
+++ src/components/projects-components/MainProject.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function MainProject(){
+    return (
+        <div className="main-project-card">
+        </div>
+    )
+}

a2cf82d5deab7c31af36abecb1e915650209fabd
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2025-09-24 18:20:26 +0200

    second project
    added second projects data

---
+++ src/components/projects-components/projects-data/second-project-data.js
@@ -0,0 +1,29 @@
+import unitConverter from "../../img/projects-img/unit-converter.png"
+import colorSchemeGenerator from "../../img/projects-img/color-scheme-generator.png"
+import passwordGenerator from "../../img/projects-img/password-generator.png"
+
+
+
+export const secondProjectsData = [
+    {
+        id: "unitConverter-01",
+        title: "Unit Converter",
+        siteLink: "https://www.tuo-sito.com/landing",
+        githubLink: "https://github.com/tuo-profilo/landing-page",
+        image: unitConverter
+    },
+    {
+        id: "passwordGenerator-02",
+        title: "Password Generator",
+        siteLink: "https://www.tuo-sito.com/landing",
+        githubLink: "https://github.com/tuo-profilo/landing-page",
+        image: passwordGenerator
+    },
+    {
+        id: "colorSheme-03",
+        title: "Color Scheme Generator",
+        siteLink: "https://www.tuo-sito.com/landing",
+        githubLink: "https://github.com/tuo-profilo/landing-page",
+        image: colorSchemeGenerator
+    },
+]

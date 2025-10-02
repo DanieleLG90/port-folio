6c931d8b56d213fdf02989a8d990db404d8fbedf
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2025-10-02 19:39:25 +0200

    slide
    tried to add a slide effect for the main project in the mobile view but got stuked and way to bugged

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MainProject from './projects-components/MainProject';
 import SecondProject from './projects-components/SecondProject';
@@ -13,12 +11,13 @@ export default function Projects(){
         <div className='projects-container'>
             
             <div className='main-projects-container'>
-                {mainProjectsData.map(project => (
-                    <MainProject 
-                        key={project.id} 
-                        project={project} 
-                    />
-                ))}
+                
+                    {mainProjectsData.map(project => (
+                            <MainProject 
+                                key={project.id} 
+                                project={project} 
+                            />
+                    ))}
             </div>
 
             <div className='secondary-projects-container'>
